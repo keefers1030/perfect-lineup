b4447f78e5ee6d1c4f5197dc455d26cae0fa94e9
Keith March Mistler <kmistler@gmail.com>
2021-04-13 08:42:24 -0400

    added functions to index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// function will return `true` if the lineup provided adheres to rules:
+
+/* 1) The total salary of all players in a lineup may not exceed $45,000
+2) Lineups may not contain more than 2 players from a single team
+3) Lineups may not contain more than 3 players from a single game
+4) Lineups must contain exactly 3 players with the position of 'OF' 
+and must also contain exactly 1 player from each of the following positions: 
+'P', 'C', '1B', '2B', '3B', 'SS' */
+
+const lineUp = () => {
+    
+}
+
+const validateLineUp = () => {
+//
+}
+
+module.exports = validateLineUp
