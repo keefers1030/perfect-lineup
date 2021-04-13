263997056fb02b17906eeae75fe87b79897596d3
Keith March Mistler <kmistler@gmail.com>
2021-04-13 08:47:01 -0400

    added pseudo code

--- index.js
+++ index.js
@@ -8,11 +8,25 @@ and must also contain exactly 1 player from each of the following positions:
 'P', 'C', '1B', '2B', '3B', 'SS' */
 
 const lineUp = () => {
-    
+    // returns false when the lineup includes too many players from a single team
+
+    // returns false when the lineup includes too many players from a single game
+
+    // returns false when the lineup includes too many players
+
+    // returns false when the lineup does not have the right number of players at each position
+
+    // returns false when the lineup includes too few players from a single position
+
+    // returns false when the lineup does not include a player from a position
+
+    // returns false when the lineup has a total salary greater than 45000
+
 }
 
 const validateLineUp = () => {
-//
+    // returns true when the lineup satisfies all conditions
+
 }
 
 module.exports = validateLineUp
